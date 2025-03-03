500999def4c79b3de055d5f91159ba2831204883
Javad <ja7ad@live.com>
2025-03-03 12:19:37 +0330

    fix: remove interface and use blockchain and asset

--- gen/golang/asset.go
+++ gen/golang/asset.go
@@ -1,19 +1,5 @@
 package golang
 
-type Blockchain interface {
-	Name() string
-	Description() string
-	Website() string
-	Explorer() string
-	Symbol() string
-	Decimals() uint
-	Links() []Link
-	Assets() []Asset
-	Asset(id string) Asset
-	// Logo return raw svg as string
-	Logo() string
-}
-
 type Asset interface {
 	ID() string
 	Name() string
