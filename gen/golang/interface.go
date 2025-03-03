3cb51d269faf44e1f506959f7501e0fd9b6bbc76
Javad <ja7ad@live.com>
2025-03-03 11:05:57 +0330

    feat: add generator golang files

---
+++ gen/golang/interface.go
@@ -0,0 +1,29 @@
+package golang
+
+type Blockchain interface {
+	Name() string
+	Description() string
+	Website() string
+	Explorer() string
+	Symbol() string
+	Decimals() uint
+	Links() []Link
+	Assets() []Asset
+	Asset(id string) Asset
+	// Logo return raw svg as string
+	Logo() string
+}
+
+type Asset interface {
+	ID() string
+	Name() string
+	Description() string
+	Symbol() string
+	Decimals() uint
+	Type() AssetType
+	Website() string
+	Explorer() string
+	Status() string
+	// Icon return raw svg as string
+	Icon() string
+}
